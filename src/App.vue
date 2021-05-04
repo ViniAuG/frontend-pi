e81dfa997f743cc5d74f40daf21f90357e63af73
Vinicius <viniciustrajano@icloud.com>
2021-05-04 18:53:15 -0300

    Adding responsive navabar and routers

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ResponsiveNavigation
+		:nav-links="navLinks"
+		:image-path="require('./assets/logo.png')"
+		background="#fff"
+		link-color="#777"
+		hoverBackground="#ddd"
+    />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ResponsiveNavigation from '@/components/ResponsiveNavigation'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    ResponsiveNavigation
+  },
+  data: () => ({
+    navLinks: [
+      {
+        text: 'Contact',
+		path: '/contact',
+		icon: 'ion-ios-megaphone'
+      },
+      {
+        text: 'About',
+		path: '/about',
+		icon: 'ion-ios-business'
+      },
+      {
+        text: 'Home',
+		path: '/home',
+		icon: 'ion-ios-bonfire'
+      },
+      {
+        text: 'Dashboard',
+		path: '/dashboard',
+		icon: 'ion-ios-briefcase'
+      }
+    ]
+  })
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css';
+
+figure {
+	margin-block-start: 0;
+	margin-block-end: 0;
+	margin-inline-start: 10px;
+	margin-inline-end: 0;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
