d189e642f82d4320f399c69416554b97556d5e9b
Vinicius <viniciuslima.4k@gmail.com>
2021-05-13 13:53:40 -0300

    About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <body>
+    <div class="block" v-responsive="{ small: block => block.width <= 500 }">
+
+      <div class="inside-block">
+        <h2 id="content-title">Sobre nós</h2>
+        <p id="content-paragraph">Estamos em nosso sétimo projeto no Centro universitário SENAC. Este, por sua vez, refere-se a uma aplicação real no mundo da produção de produtos. Nosso produto visa facilitar a vida dos produtores por meio de tecnologias aplicadas e uma alta gama de ferramentas disponíveis para uso diário.</p>
+        <img src="../assets/senac.jpg" id="production-image" class="production-image"  v-responsive="{ small: el => el.width <= 500 }">
+      </div>
+
+    </div>
+  </body>
 </template>
 
 <script>
@@ -10,3 +18,42 @@ export default {
   name: 'about'
 }
 </script>
+
+<style scoped>
+
+.inside-block {
+  display: inline-block;
+  vertical-align: middle;
+  height: 500px;
+  width: 800px;
+}
+
+#content-title {
+  font-size: 30px;
+  color: aliceblue;
+  margin-bottom: 20px;
+}
+
+#content-paragraph {
+  font-size: 15px;
+  text-align: center;
+  color: aliceblue;
+}
+
+#production-image{
+  display: inline-block;
+  vertical-align: middle;
+  height: 600px;
+  width: 800px;
+}
+
+body {
+  overflow: auto;
+  padding-top: 10px;
+  margin-top: 0;
+  margin-bottom: 0;
+  background-color: #1B0C2E;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
